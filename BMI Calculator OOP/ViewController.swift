56bfa7f9b4aab4d2d66a8d3133317001a4f2145c
jscheth <sovichea.chet@gmail.com>
2025-04-07 14:12:18 -0700

    Initial Commit

---
+++ BMI Calculator OOP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator OOP
+//
+//  Created by Jonathan Cheth on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMI Calculator OOPTests/BMI_Calculator_OOPTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_Calculator_OOPTests.swift
+//  BMI Calculator OOPTests
+//
+//  Created by Jonathan Cheth on 4/7/25.
+//
+
+import Testing
+@testable import BMI_Calculator_OOP
+
+struct BMI_Calculator_OOPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
